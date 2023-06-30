40091e31a9ca6a04cf137c644ec1983ae217ab84
Boris Simonov <boris.simonov@p2p.org>
2023-06-30 11:19:42 +0300

    PK-1572: add events exporter

---
+++ exporters/events/internal/client/wsbroker.go
@@ -0,0 +1,219 @@
+package client
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"substrate-events-exporter/internal/dto"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+	"nhooyr.io/websocket"
+)
+
+type WSMessageBroker struct {
+	//in            chan *WSMessageBrokerFrame
+	log           *logrus.Logger
+	requests      sync.Map // map[int]chan *WSMessageBrokerFrame
+	subscriptions sync.Map // map[string]chan *WSMessageBrokerFrame
+	ws            *websocket.Conn
+	ctx           context.Context
+	err           error
+}
+
+type WSMessageBrokerFrame struct {
+	Frame *dto.RPCFrame
+	Error error
+	ctx   context.Context
+}
+
+func NewWSMessageBroker(ctx context.Context, log *logrus.Logger, conn *websocket.Conn) *WSMessageBroker {
+	broker := WSMessageBroker{
+		log: log,
+		ctx: ctx,
+		ws:  conn,
+	}
+
+	go func() {
+		broker.err = broker.readContinuously(ctx)
+	}()
+	return &broker
+}
+
+// Simple request wihtout need to watch server-side pushes
+func (broker *WSMessageBroker) WSRequest(ctx context.Context, req *dto.RPCFrame) (*dto.RPCFrame, error) {
+	ch, err := broker.submit(ctx, req, false)
+	if err != nil {
+		return nil, err
+	}
+	select {
+	case brokerResp := <-ch:
+		if brokerResp.Error == nil {
+			return brokerResp.Frame, nil
+		} else {
+			return nil, brokerResp.Error
+		}
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+
+}
+
+// Submit and subscribe on server pushes
+func (broker *WSMessageBroker) Subscribe(ctx context.Context, req *dto.RPCFrame) (chan *dto.RPCFrame, chan error) {
+	r := make(chan *dto.RPCFrame)
+	e := make(chan error, 1)
+	ch, err := broker.submit(ctx, req, true)
+	if err != nil {
+		e <- err
+		return nil, e
+	}
+
+	go func() {
+		defer close(r)
+		defer close(e)
+		for frame := range ch {
+			ctx = frame.ctx
+			if frame.Error != nil {
+				e <- frame.Error
+				return
+			}
+			r <- frame.Frame
+		}
+	}()
+	return r, e
+}
+
+func (broker *WSMessageBroker) submit(ctx context.Context, frame *dto.RPCFrame, isSubscription bool) (chan *WSMessageBrokerFrame, error) {
+	frame.RPC = "2.0"
+	if frame.Id == 0 {
+		frame.Id = 1 + rand.Intn(29999)
+	}
+	if frame.Params == nil {
+		frame.Params = []string{}
+	}
+	payload, err := json.Marshal(frame)
+	if err != nil {
+		return nil, err
+	}
+	err = broker.ws.Write(ctx, websocket.MessageText, payload)
+	if err != nil {
+		return nil, err
+	}
+	var ch chan *WSMessageBrokerFrame
+	if isSubscription {
+		broker.log.Debugf("new subscription id: %d, method %s", frame.Id, frame.Method)
+		ch = make(chan *WSMessageBrokerFrame)
+		broker.subscriptions.Store(subscriptionInitKey(frame.Id), ch)
+	} else {
+		broker.log.Debugf("new request id: %d, method %s", frame.Id, frame.Method)
+		ch = make(chan *WSMessageBrokerFrame, 1)
+		broker.requests.Store(frame.Id, ch)
+	}
+	return ch, nil
+}
+
+// Temporary key to init subscription
+func subscriptionInitKey(frameId int) string {
+	return fmt.Sprintf("init/%d", frameId)
+}
+
+func (broker *WSMessageBroker) cleanup() {
+	broker.requests.Range(func(k, v interface{}) bool {
+		ch := v.(chan *WSMessageBrokerFrame)
+		ch <- &WSMessageBrokerFrame{
+			Frame: &dto.RPCFrame{},
+			Error: broker.err,
+		}
+		close(ch)
+		return true
+	})
+	broker.subscriptions.Range(func(k, v interface{}) bool {
+		ch := v.(chan *WSMessageBrokerFrame)
+		ch <- &WSMessageBrokerFrame{
+			Frame: &dto.RPCFrame{},
+			Error: broker.err,
+		}
+		close(ch)
+		return true
+	})
+}
+
+func (broker *WSMessageBroker) readContinuously(ctx context.Context) error {
+	// read loop
+	read := make(chan *WSMessageBrokerFrame)
+	defer broker.cleanup()
+	go func(ctx context.Context, read chan *WSMessageBrokerFrame) {
+		defer close(read)
+		for {
+			var (
+				err error
+				buf []byte
+			)
+			if ctx.Err() != nil {
+				return
+			}
+			if _, buf, err = broker.ws.Read(ctx); err != nil {
+				read <- &WSMessageBrokerFrame{
+					ctx:   ctx,
+					Frame: nil,
+					Error: err,
+				}
+				continue
+			}
+			var f dto.RPCFrame
+			if err = json.Unmarshal(buf, &f); err != nil {
+				broker.log.WithError(err).Errorf("failed to parse server-side frame %s", string(buf))
+				continue
+			}
+			read <- &WSMessageBrokerFrame{
+				ctx:   context.WithValue(ctx, ContextKeyRPCCallId, f.Id),
+				Frame: &f,
+				Error: nil,
+			}
+		}
+	}(ctx, read)
+
+	for {
+		select {
+		case frame := <-read:
+			// frame not read
+			if frame.Error != nil {
+				// read loop or pinger error -> stop processing
+				return frame.Error
+			}
+			//var resp *WSMessageBrokerFrame
+			// server-side subscription push does not contain Id
+			if frame.Frame.Id == 0 {
+				pushParams := dto.MustMap(frame.Frame.Params)
+				subscriptionID := pushParams.MustString("subscription")
+				if ch, ok := broker.subscriptions.Load(subscriptionID); ok {
+					ch := ch.(chan *WSMessageBrokerFrame)
+					ch <- &WSMessageBrokerFrame{
+						Error: frame.Error,
+						Frame: frame.Frame,
+					}
+				} else {
+					broker.log.Warnf("no response channel found for subscription %d", subscriptionID)
+				}
+			} else {
+				// move subscription from init/123 to it's place
+				if ch, initExists := broker.subscriptions.LoadAndDelete(subscriptionInitKey(frame.Frame.Id)); initExists {
+					subscriptionId := frame.Frame.StringResult()
+					broker.subscriptions.Store(subscriptionId, ch)
+				} else {
+					if ch, ok := broker.requests.LoadAndDelete(frame.Frame.Id); ok {
+						ch := ch.(chan *WSMessageBrokerFrame)
+						ch <- &WSMessageBrokerFrame{
+							Error: frame.Error,
+							Frame: frame.Frame,
+						}
+					}
+				}
+			}
+		case <-ctx.Done():
+			return ctx.Err()
+		}
+	}
+}
---
+++ exporters/events/internal/dto/dto.go
@@ -0,0 +1,175 @@
+package dto
+
+import (
+	"encoding/json"
+	"strconv"
+)
+
+type Params interface{}
+
+type RPCFrame struct {
+	Id     int         `json:"id"`
+	RPC    string      `json:"jsonrpc"`
+	Method string      `json:"method"`
+	Raw    string      `json:"raw,omitempty"`
+	Params Params      `json:"params,omitempty"`
+	Result interface{} `json:"result,omitempty"`
+	Error  *RPCError   `json:"error,omitempty"`
+}
+
+func NewFrameFrom(payload []byte) (*RPCFrame, error) {
+	var f RPCFrame
+	if err := json.Unmarshal(payload, &f); err != nil {
+		return nil, err
+	}
+	f.Raw = string(payload)
+	return &f, nil
+}
+
+func (r *RPCFrame) MappedResult() Mapped {
+	switch v := r.Result.(type) {
+	case map[string]interface{}:
+		return v
+	default:
+		return Mapped{}
+	}
+}
+
+func (r *RPCFrame) StringResult() string {
+	switch v := r.Result.(type) {
+	case string:
+		return v
+	default:
+		return ""
+	}
+}
+
+type RPCError struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type Block struct {
+	Raw        string
+	Number     uint64
+	ParentHash string
+	Hash       string
+	Extrinsics []string
+}
+
+type Event struct {
+	Type         EventType   `json:"type"`
+	ExtrinsicIdx int         `json:"extrinsic_idx"`
+	Params       interface{} `json:"params"`
+}
+
+type EventType struct {
+	ModuleId string `json:"module_id"`
+	EventId  string `json:"event_id"`
+}
+
+type Mapped map[string]interface{}
+
+func MustMap(p interface{}) Mapped {
+	switch v := p.(type) {
+	case map[string]interface{}:
+		return v
+	default:
+		return Mapped{}
+	}
+}
+
+func (r Mapped) MustStringMap(key string) map[string]string {
+	res := make(map[string]string)
+	switch v := r[key].(type) {
+	case map[string]interface{}:
+		for k, item := range v {
+			if val, ok := item.(string); ok {
+				res[k] = val
+			} else {
+				res[k] = ""
+			}
+		}
+		return res
+	default:
+		return res
+	}
+}
+
+func (r Mapped) MustInt(key string) uint64 {
+	switch v := r[key].(type) {
+	case string:
+		if len(v) >= 2 && (v[:2] == "0x" || v[:2] == "0X") {
+			v, _ := strconv.ParseUint(v[2:], 16, 64)
+			return v
+		}
+		return 0
+	case uint64:
+		return v
+	case float64:
+		return uint64(v)
+	default:
+		return 0
+	}
+}
+
+func (r Mapped) Get(key string) Mapped {
+	switch v := r[key].(type) {
+	case map[string]interface{}:
+		return v
+	default:
+		return Mapped{}
+	}
+}
+
+func (r Mapped) GetInterface(key string) interface{} {
+	if v, ok := r[key]; ok {
+		return v
+	} else {
+		return nil
+	}
+}
+
+func (r Mapped) GetSlice(key string) []Mapped {
+	switch v := r[key].(type) {
+	case []interface{}:
+		var res []Mapped
+		for i := 0; i < len(v); i++ {
+			rm, ok := v[i].(map[string]interface{})
+			if ok {
+				res = append(res, rm)
+			} else {
+				res = append(res, Mapped{})
+			}
+		}
+		return res
+	default:
+		return []Mapped{}
+	}
+}
+
+func (r Mapped) MustStrings(key string) []string {
+	switch v := r[key].(type) {
+	case []interface{}:
+		var res []string
+		for _, item := range v {
+			_, ok := item.(string)
+			if ok {
+				res = append(res, item.(string))
+			}
+		}
+		return res
+	default:
+		return []string{}
+	}
+}
+
+func (r Mapped) MustString(key string) string {
+	n, ok := r[key].(string)
+	if ok {
+		return n
+	} else {
+		return ""
+	}
+}
