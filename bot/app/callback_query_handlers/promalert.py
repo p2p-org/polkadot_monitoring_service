9a76c55b6e7b87fe03d21a91d27a2031f424e715
base1217 <anton.zh@p2p.org>
2023-08-21 11:54:11 +0300

    Inline menu(start)

--- bot/app/callback_query_handlers/promalert.py
+++ bot/app/callback_query_handlers/promalert.py
@@ -1,19 +1,41 @@
-from __main__ import router,dp,db,bot,cb
+from __main__ import dp, db, bot
+from aiogram.utils.callback_data import CallbackData
 from aiogram.types import CallbackQuery,InlineKeyboardButton,InlineKeyboardMarkup
-from aiogram import F
-from utils.menu_builder import MenuBuilder
 
+cb = CallbackData('row', 'action')
 
-@router.callback_query(cb.filter(F.dst == 'promalert'))
-async def menu_prom_cb_handler(query: CallbackQuery,callback_data: cb):
-    await query.answer(query.id)
+@dp.callback_query_handler(cb.filter(action='promalert_menu'))
+async def menu_prom_cb_handler(query: CallbackQuery):
+    chat_id = query['from']['id']
+    message_id = query['message']['message_id']
+
+    keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton(text="Set", callback_data=cb.new(action='promalert_set')),
+                                          InlineKeyboardButton(text="Deactivate", callback_data=cb.new(action='promalert_deactivate')))
     
-    username = query.message.chat.username
-    chat_id = query.message.chat.id
-    message_id = query.message.message_id
+    await bot.send_message(chat_id,"You can subscribe or deactivate subsription hete.",reply_markup=keyboard)
+    await query.answer(query.id)
 
-    menu = MenuBuilder()
-    menu = menu.build(callback_data=cb,preset='promalert',button_back='main_menu')
+@dp.callback_query_handler(cb.filter(action='promalert_set'))
+async def menu_prom_cb_handler(query: CallbackQuery):
+    chat_id = query['from']['id']
+    message_id = query['message']['message_id']
 
-    await bot.send_message(chat_id,"You can subscribe or deactivate subsription hete.",reply_markup=menu.as_markup())
     await bot.delete_message(chat_id,message_id)
+    
+    db.update_record(chat_id,'promalert_status','on')
+    
+    await bot.send_message(chat_id,"Alright!\n\nFrom now you able to receive predefined alerts.")
+    
+    await query.answer(query.id)
+
+@dp.callback_query_handler(cb.filter(action='promalert_deactivate'))
+async def menu_prom_cb_handler(query: CallbackQuery):
+    chat_id = query['from']['id']
+    message_id = query['message']['message_id']
+
+    await bot.delete_message(chat_id,message_id)
+
+    db.update_record(chat_id,'promalert_status','off')
+
+    await bot.send_message(chat_id,"Your subscriprion has been deactivated.")
+    await query.answer(query.id) 
--- bot/app/message_handlers/start.py
+++ bot/app/message_handlers/start.py
@@ -1,7 +1,6 @@
-from __main__ import router,dp,db,bot,admin_chat,cb
-from utils.menu_builder import MenuBuilder
-from aiogram.types import Message
-from aiogram import F
+from __main__ import dp, db, bot, admin_chat
+from callback_query_handlers import promalert,grafana
+from aiogram.types import CallbackQuery,Message,InlineKeyboardButton,InlineKeyboardMarkup
 
 @router.message(F.content_type.in_({'text', 'sticker','photo'}))
 async def command_start(message: Message) -> None:
@@ -20,10 +19,20 @@ async def command_start(message: Message) -> None:
         await message.answer(chat_id,"Your account has been disabled 🤷\nSorry and have a good day.")
         return
 
+    if account_status and account_status == 'off':
+        await message.answer(chat_id,"Your account has been disabled 🤷\nSorry and have a good day.")
+        return
+
+    menu = InlineKeyboardMarkup()
+    buttons = [InlineKeyboardButton(text="Operate over Grafana instances", callback_data=grafana.cb.new(func='grafana_instances',action="")),
+               InlineKeyboardButton(text="Operate over Prometheus alerts", callback_data=promalert.cb.new(action='promalert_menu')),
+               InlineKeyboardButton(text="Contact us(support)", callback_data=promalert.cb.new(action='support'))]
+
+    for button in buttons:
+        menu.row(button)
+    
     if not account_status:
         await bot.send_message(admin_chat, "Username: @{} ID: {}\nHas just PRE-registered.".format(username,chat_id))
         db.add_account(chat_id,username)
-        
-        await message.answer("Hi there 👋\n\n\nWelcome to a validator monitoring bot by P2P.org\n\n\n\n",reply_markup=menu.as_markup())
-    else:
-        await message.answer("Here is main menu",reply_markup=menu.as_markup())
+
+    await message.answer("Hi there 👋\n\n\nWelcome to a validator monitoring bot by P2P.org\n\n\n\n",reply_markup=menu)
