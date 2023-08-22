58b274dff07b5bc06cd688b1bb6f39ca82dba13e
sergeyradchenkop2p <sergey.radchenko@p2p.org>
2023-08-22 11:41:19 +0300

    moonbeam common exporter for MaaS

--- exporters/moonbeam/exporter.py
+++ exporters/moonbeam/exporter.py
@@ -57,7 +57,7 @@ def metrics():
         out += '# HELP moonbeam_blocksMedian Blocks median\n'
         out += '# TYPE moonbeam_blocksMedian Blocks median counter\n'
 
-        out += 'moonbeam_blocksMedian{chain="%s"} %s\n' % (chain,metrics['common']['median'])
+        out += 'moonbeam_blocksMedian{chain="%s" } %s\n' % (chain,metrics['common']['median'])
     except KeyError:
         pass
 
@@ -70,32 +70,14 @@ def metrics():
         pass
 
     try:
-        out += '# HELP moonbeam_activeCollators Active collator\n'
-        out += '# TYPE moonbeam_activeCollators Active collator counter\n'
-
-        for k,v in metrics['collators'].items():
-            out += 'moonbeam_activeCollators{node="%s",chain="%s",account="%s"} %s\n' % (v['node'],chain,k,v['is_active'])
-    except KeyError:
-        pass
-
-    try:
         out += '# HELP moonbeam_blockAuthorship Block authors\n'
         out += '# TYPE moonbeam_blockAuthorship Block authors counter\n'
 
         for k,v in metrics['collators'].items():
-            out += 'moonbeam_blockAuthorship{node="%s",chain="%s",account="%s"} %s\n' % (v['node'],chain,k,v['authored_blocks_count'])
+            out += 'moonbeam_blockAuthorship{chain="%s",account="%s"} %s\n' % (chain,k,v)
     except KeyError:
         pass
  
-    try:
-        out += '# HELP moonbeam_activesetPosition Active set position\n'
-        out += '# TYPE moonbeam_activesetPosition Active set position\n'
-
-        for k,v in metrics['collators'].items():
-            out += 'moonbeam_activesetPosition{node="%s",chain="%s",account="%s"} %s\n' % (v['node'],chain,k,v['set_position'])
-    except KeyError:
-        pass
-
     try:
         out += '# HELP moonbeam_rooundProgress Round progress\n'
         out += '# TYPE moonbeam_roundProgress Round progress counter\n'
@@ -149,13 +131,6 @@ def get_round_progress(chain):
 
     return round_progress
 
-
-def get_activeset_position(account):
-    candidate_pool = api_request(method = 'api.query.parachainStaking.candidatePool')
-    sorted_pool = sorted(candidate_pool, key=lambda i: i['amount'], reverse = True)
-    set_index = next((index for (index, d) in enumerate(sorted_pool) if d["owner"] == account), None) + 1
-    return set_index
-
 def get_config(part):
     with open('./config.yaml') as config_file:
         data = yaml.load(config_file, Loader=yaml.FullLoader)
@@ -177,43 +152,32 @@ def main():
                 common['active_collators'] = len(active_collators)
                 common['current_round'] = current_rnd
                 common['rnd_blocks'] = 0
-                
-                for k,v in collators.items():
-                    v['authored_blocks_count'] = 0
-                    v['is_disabled'] = 0
-                    if k in active_collators:
-                        v['is_active'] = 1
-                        v['set_position'] = get_activeset_position(k)
-                    else:
-                        v['is_active'] = 0
-                        v['set_position'] = 0
-                
-                other_collators = {k:0 for k in active_collators if k not in collators.keys()}
-
-                result = {'collators':collators, 'common':common}
+                authored_blocks_count = 0
+                all_collators = {k:authored_blocks_count for k in active_collators}
+                result = {'collators':all_collators, 'common':common}
                 logging.info('New round ' + str(current_rnd) + ' has just begun')
-
+                
             last_block = int(api_request(method = 'api.query.system.number'),16)
-
-            if last_block != block:
+            
+            if  last_block != block:
                 logging.info('Processing block ' + str(last_block))
-                   
+               
                 block_author = api_request(method = 'api.query.authorInherent.author')
                                     
-                if block_author in other_collators.keys():
-                    other_collators[block_author] += 1
- 
                 for addr,params in result['collators'].items():
+                                                         
                     if block_author == addr:
-                        params['authored_blocks_count'] += 1
-                        logging.info('Collator ' + str(addr) + ' has just constructed block ' + str(last_block))
+                       result['collators'][addr] += 1
+                       
+                        
+                       logging.info('Collator ' + str(addr) + ' has just constructed block ' + str(last_block))
                 
                 result['common']['round_progress'] = int(get_round_progress(get_config('chain')))
                 result['common']['rnd_blocks'] += 1
 
-                result['common']['median'] = int(Decimal(median(list(other_collators.values()))))
-                result['common']['average'] = int(Decimal(average(list(other_collators.values()))))
-                result['common']['p95'] = int(Decimal(percentile(list(other_collators.values()),95)))
+                result['common']['median'] = int(Decimal(median(list(all_collators.values()))))
+                result['common']['average'] = int(Decimal(average(list(all_collators.values()))))
+                result['common']['p95'] = int(Decimal(percentile(list(all_collators.values()),95)))
                 
                 q_metrics.clear()
                 q_metrics.append(result)
@@ -234,8 +198,8 @@ if __name__ == '__main__':
        
     collators = {}
 
-    for k,v in get_config('validators').items():
-        collators[v['account']] = {'node':k}
+   # for k,v in get_config('validators').items():
+   #     collators[v['account']] = {'node':k}
 
     q_metrics = deque([])
 
