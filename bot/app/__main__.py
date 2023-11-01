3a9ed0614ccd0daf3854e9d346a0da0b40ee50cd
base1217 <anton.zh@p2p.org>
2023-11-01 16:40:17 +0200

    Upgrade to aiogram3

--- bot/app/__main__.py
+++ bot/app/__main__.py
@@ -4,7 +4,6 @@ import os
 import asyncio
 from aiogram import Bot, Dispatcher, Router
 from aiogram.fsm.storage.memory import MemoryStorage
-#from aiogram.utils import executor
 from message_handlers.setup import setup_message_handler
 from web_apps.setup import setup_web_app
 from forms.setup import setup_message_form
@@ -12,6 +11,7 @@ from callback_data.main import Cb
 from aiohttp import web
 from db import DB
 import time 
+
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
 if __name__ == "__main__":
@@ -26,19 +26,18 @@ if __name__ == "__main__":
     
     run_mode = os.environ.get('run_mode', 'standalone')
 
-    #loop = asyncio.get_event_loop()
-    loop = asyncio.get_event_loop()
-    storage = MemoryStorage()
-    #bot = Bot(token=tg_token, loop=loop, parse_mode="HTML")
-    bot = Bot(token=tg_token, parse_mode="HTML")
-    #dp = Dispatcher(bot, storage=storage)
-    dp = Dispatcher(storage=storage)
-    router = Router()
-    dp.include_router(router)
     web_app = web.Application()
     db = DB(db_name,db_user,db_pass,db_host,db_port)
 
     bot = Bot(token=tg_token, parse_mode="HTML")
+
+    storage = MemoryStorage()
+    dp = Dispatcher(storage=storage)
+    
+    router = Router()
+    dp.include_router(router)
+
+    setup_message_handler('start')
 
     storage = MemoryStorage()
     dp = Dispatcher(storage=storage)
@@ -64,5 +63,5 @@ if __name__ == "__main__":
     
     site = web.TCPSite(web_runner, port=8080)
     loop.run_until_complete(site.start())
-    #executor.start_polling(dp, skip_updates=True)
-#    dp.run_polling(bot)
+
+    loop.run_until_complete(dp.start_polling(bot))
